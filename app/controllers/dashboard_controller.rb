12ba5d7790b2fcbd0e1fdd211e0ba2ed9753f3ac
mar bautista <marilirulita@gmail.com>
2024-04-02 14:45:35 -0700

    add last functions and colors in dashboard tble

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    @withheld_totals = WithheldTax.select("sum(income_withheld) as total_income, sum(iva_withheld) as total_iva, sum(isr_withheld) as total_isr")
+    @income_total = Income.select("sum(amount) as total_amount")
+    @expense_total = Expense.select("sum(amount) as total_amount")
   end
 end
